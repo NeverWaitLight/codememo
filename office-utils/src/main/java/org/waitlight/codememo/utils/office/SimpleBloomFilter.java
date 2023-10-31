c05e73b6cfb4b6f9fb2ec61f7df716e033b45d35
denglei <deng.lei@yilungroup.com>
2023-10-31 15:23:11 +0800

    bloom filter

---
+++ office-utils/src/main/java/org/waitlight/codememo/utils/office/SimpleBloomFilter.java
@@ -0,0 +1,49 @@
+package org.waitlight.codememo.utils.office;
+
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.BitSet;
+
+public class SimpleBloomFilter {
+
+    private final int bitSetSize;
+    private BitSet bitset;
+    private final int hashFunctionCount;
+    private final MessageDigest[] digestFunctions;
+
+    public SimpleBloomFilter(int bitSetSize, int hashFunctionCount) throws NoSuchAlgorithmException {
+        this.bitSetSize = bitSetSize;
+        this.bitset = new BitSet(bitSetSize);
+        this.hashFunctionCount = hashFunctionCount;
+        this.digestFunctions = new MessageDigest[hashFunctionCount];
+        for (int i = 0; i < hashFunctionCount; i++) {
+            digestFunctions[i] = MessageDigest.getInstance("MD5");
+        }
+    }
+
+    public void add(String value) {
+        for (int i = 0; i < hashFunctionCount; i++) {
+            int hashValue = getHash(value, i);
+            bitset.set(Math.abs(hashValue) % bitSetSize, true);
+        }
+    }
+
+    public boolean contains(String value) {
+        for (int i = 0; i < hashFunctionCount; i++) {
+            int hashValue = getHash(value, i);
+            if (!bitset.get(Math.abs(hashValue) % bitSetSize)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getHash(String value, int index) {
+        MessageDigest md = digestFunctions[index];
+        md.update(value.getBytes());
+        byte[] hashBytes = md.digest();
+        ByteBuffer byteBuffer = ByteBuffer.wrap(hashBytes);
+        return byteBuffer.getInt();
+    }
+}
---
+++ office-utils/src/test/java/org/waitlight/codememo/utils/office/SimpleBloomFilterTest.java
@@ -0,0 +1,21 @@
+package org.waitlight.codememo.utils.office;
+
+import org.junit.jupiter.api.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleBloomFilterTest {
+
+    @Test
+    void contains() throws NoSuchAlgorithmException {
+        SimpleBloomFilter f = new SimpleBloomFilter(1024, 3);
+        f.add("tom");
+        f.add("jerry");
+
+        assertTrue(f.contains("tom"));
+        assertFalse(f.contains("tom1"));
+    }
+}
